50294c62c85753c62ad9e24ff33ff2f14099d4d5
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-12-30 23:31:42 +0100

    Store & process commands in custom variable

--- elmacro.el
+++ elmacro.el
@@ -6,63 +6,44 @@
 ;; TODO M-x elmacro-set-logging 'always or 'on-macro
 ;; TODO extract macro with start-kbd-macro instead of kmacro-start-macro?
 ;; TODO (defvar elmacro-store-all-commands 'always)
-;; TODO see the todo in defadvice call-interactively
 ;; TODO MELPA / Cask?
 
 (require 's)
 (require 'dash)
 
-(setq history-length t)
+(defvar elmacro-recorded-commands '())
+(defvar elmacro-commands-with-input '(isearch-printing-char isearch-other-control-char))
 
-(defadvice call-interactively (around elmacro-store-command last (func &optional record keys) activate)
-  "Always save whatever is called interactively in `command-history' and process it."
-  (setq record t)
-  ad-do-it
-  ;; TODO copy last command into `elmacro-commands' and process this instead.
-  ;; The problem we currently have is commands like (smex) which
-  ;; appear in command-history BUT NOT when copied... also some
-  ;; commands don't appear the same... probably because of the magic
-  ;; happening in ido-ubiquitous. Figure what happens there first.
-  (elmacro-process-last-command 'command-history))
+(defun elmacro-process-latest-command ()
+  "Process the latest command of variable `command-history' into `elmacro-recorded-commands'."
+  (--each (elmacro-transform-command (car command-history))
+    (!cons it elmacro-recorded-commands)))
 
-(defun elmacro-handle-self-insert-command (list-var)
-  "Replace (self-insert-command 1) with (insert \"x\")."
-  (setcar (symbol-value list-var) `(insert ,(string last-command-event))))
+(defun elmacro-transform-command (cmd)
+  "Process CMD into something more suitable if needed."
+  (let* ((func (car cmd))
+         (commands-with-input '(isearch-printing-char isearch-other-control-char)))
+    (cond ((equal func 'self-insert-command)
+           `((insert ,(string last-command-event))))
+          ((-contains? commands-with-input func)
+           `((setq last-command-event ,last-command-event)
+             ,cmd))
+          ((eq t t)
+           `(,cmd)))))
 
-(defun elmacro-handle-isearch-printing-char (list-var)
-  "FIXME"
-  (set list-var (-insert-at
-                 1
-                 `(setq last-command-event ,last-command-event)
-                 (symbol-value list-var))))
-
-(defun elmacro-handle-isearch-other-control-char (list-var)
-  "FIXME"
-  (set list-var (-insert-at
-                 1
-                 `(setq last-command-event ,last-command-event)
-                 (symbol-value list-var))))
-
-(defun elmacro-process-last-command (list-var)
-  "Process the latest command of LIST-VAR into something more suitable."
-  (let* ((func (-first-item (-first-item (symbol-value list-var))))
-         (handler (intern (format "elmacro-handle-%s" func))))
-    (if (fboundp handler)
-        (funcall handler list-var))))
-
-(defun elmacro-extract-last-kbd-macro (lst)
-  "Extracts the last keyboard macro from LST."
+(defun elmacro-extract-last-kbd-macro (commands)
+  "Extract the last keyboard macro from COMMANDS."
   (-drop 1 (--take-while (not (equal it '(kmacro-start-macro nil)))
                          (--drop-while (not (equal it '(kmacro-end-macro nil)))
-                                       lst))))
+                                       commands))))
 
 (defun elmacro-get-window-object (number)
-  ""
+  "Return the window object numbered NUMBER."
   (--first (s-match (format "#<window %s[^>]+>" number) (prin1-to-string it))
            (window-list)))
 
 (defun elmacro-prin1-to-string (obj)
-  "Redefinition of prin1-to-string in order to serialize buffers, windows, etc."
+  "Print OBJ like `prin1-to-string' but handle windows, buffers, etc."
   (let* ((str (prin1-to-string obj))
          (str (replace-regexp-in-string "#<window \\([0-9]+\\)[^>]+>" "(elmacro-get-window-object \\1)" str))
          (str (replace-regexp-in-string "(quote" "(backquote" str)))
@@ -85,9 +66,15 @@
 (defun elmacro-show-last-macro (name)
   "Show the last macro as elisp."
   (interactive "sMacro name: ")
-  (elmacro-show-defun name (reverse (elmacro-extract-last-kbd-macro command-history))))
+  (elmacro-show-defun name (reverse (elmacro-extract-last-kbd-macro elmacro-recorded-commands))))
 
 (defun elmacro-edit-lossage ()
   "Show lossage as elisp."
   (interactive)
-  (elmacro-show-defun "lossage" (reverse (-take 300 command-history))))
+  (elmacro-show-defun "lossage" (reverse (-take 300 elmacro-recorded-commands))))
+
+(defadvice call-interactively (before elmacro-record-command (func &optional record keys) activate)
+  "Always save whatever is called interactively in `command-history'."
+  (setq record t))
+
+(add-hook 'post-command-hook 'elmacro-process-latest-command)
